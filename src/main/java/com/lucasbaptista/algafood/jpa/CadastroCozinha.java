f992c5b99fcf3052a7265715a6038bc9055ababb
Lucas Fernandes <lucas94_100@hotmail.com>
2020-08-01 12:20:23 -0300

    Adicionando um objeto no banco de dados

--- src/main/java/com/lucasbaptista/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/lucasbaptista/algafood/jpa/CadastroCozinha.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lucasbaptista.algafood.domain.model.Cozinha;
 
@@ -20,4 +21,9 @@ public class CadastroCozinha {
 		TypedQuery<Cozinha> query = manager.createQuery("from Cozinha", Cozinha.class);
 		return query.getResultList();
 	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}  
 }
