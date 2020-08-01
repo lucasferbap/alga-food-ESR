73532d691600ecdfa3cb5d638428113767035213
Lucas Fernandes <lucas94_100@hotmail.com>
2020-08-01 12:06:45 -0300

    Consultando Objetos do banco de dados

---
+++ src/main/java/com/lucasbaptista/algafood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.lucasbaptista.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.lucasbaptista.algafood.AlgafoodApiApplication;
+import com.lucasbaptista.algafood.domain.model.Cozinha;
+
+public class ConsultaCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
+
+}
